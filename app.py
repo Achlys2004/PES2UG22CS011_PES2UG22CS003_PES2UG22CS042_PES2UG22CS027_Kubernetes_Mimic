58e12983d29ef832b72f05ab76ae1988d50569b0
Ajay15305 <143072193+Ajay15305@users.noreply.github.com>
2025-03-30 12:22:01 +0530

    Add files via upload
    health status of all nodes and manual updates of the health status

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
+
 from flask import Flask, request, jsonify
 from sqlalchemy import text
 from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
 from models import data, Pod, Node
 from routes.nodes import nodes_bp
 from routes.pods import pods_bp
-
 app = Flask(__name__)
-
-# Load Configuration
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
-
+ 
+ 
 # Initialize Database
 data.init_app(app)
-
-# Register Blueprints with URL Prefixes
+ 
+ # Register Blueprints with URL Prefixes
 app.register_blueprint(nodes_bp, url_prefix="/nodes")
 app.register_blueprint(pods_bp, url_prefix="/pods")
-
+ 
+ 
 @app.route("/")
 def home():
     return "Kube_9 API is running!"
-
+ 
+ 
 @app.route("/test_db")
 def test_db():
     try:
         with app.app_context():
-            data.session.execute(text("SELECT 1"))
+             data.session.execute(text("SELECT 1"))
         return "Database Connected!"
     except Exception as e:
         return f"Database Connection failed: {str(e)}"
-
+ 
+ 
 if __name__ == "__main__":
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Aathil:orchid123@localhost/cluster_db'
+# config.py
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123@localhost/cluster_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
