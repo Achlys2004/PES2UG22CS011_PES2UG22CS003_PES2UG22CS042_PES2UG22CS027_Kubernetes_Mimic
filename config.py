02ab56ce5e4ba9d92d2410786f188ecd5a83a322
Aathil <aathilnishad@gmail.com>
2025-04-04 10:44:26 +0530

    some commenst

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Aathil:orchid123@localhost/cluster_db'Ṭ
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Aathil:orchid123@localhost/cluster_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
