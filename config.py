b0ab34b5803325f30743b223acf63a2eda37b0f1
Adhvaith Rajesh <rajesh.adhvaith@gmail.com>
2025-04-06 18:52:09 +0530

    simulate nodes with docker

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Aathil:orchid123@localhost/cluster_db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:dabatase@localhost/cluste_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ node_simulation/node_simulator.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+node_state = {
+    "name": "node-1",
+    "cpu_cores_avail": 4,
+    "health_status": "healthy"
+}
+
+@app.route("/status", methods=["GET"])
+def status():
+    return jsonify(node_state)
+
+@app.route("/simulate_failure", methods=["POST"])
+def simulate_failure():
+    node_state["health_status"] = "unhealthy"
+    return jsonify({"msg": "Node marked unhealthy"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
